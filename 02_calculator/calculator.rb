7f8c6ee270011149673ebc77a16467739d23c289
Greg Parsons <greg@bang.net.nz>
2019-07-25 14:51:20 +1200

    Assignment 3

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,23 @@
-#write your code here
+def add(term1, term2)
+	term1 + term2
+end
+
+def subtract(term1, term2)
+	term1 - term2
+end
+
+def sum(terms)
+	terms.reduce(0, :+)
+end
+
+def multiply(terms)
+	terms.reduce(1, :*)
+end
+
+def power(term, exponent)
+	term ** exponent
+end
+
+def factorial(term)
+	(1..term).reduce(1, :*) 
+end
