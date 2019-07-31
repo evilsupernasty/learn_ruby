2a7d50171d45b2d39577277eca9df35a64cbdd7d
Greg Parsons <greg@bang.net.nz>
2019-07-31 12:20:34 +1200

    Assignment 5

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,17 @@
-#write your code here
+def translate(phrase)
+	words = phrase.split(" ")
+
+	words.map! do |word|
+		if(word =~ /^[aeiou]/i)
+			word << 'ay'
+		elsif(word =~ /^[^aeiou]qu{1}/i || word =~ /^[^aeiou]{3}/i)
+			word[3..word.length] << word[0..2] << 'ay'
+		elsif(word =~ /^qu{1}/i || word =~ /^[^aeiou]{2}/i)
+			word[2..word.length] << word[0..1] << 'ay'
+		else
+			word[1..word.length] << word[0] << 'ay'
+		end
+	end
+
+	words.join(" ");
+end
